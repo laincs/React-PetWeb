fa27935670d33bd4d04b7eb1fcbf6a876cfe7e71
Liân <liangrandon@gmail.com>
2023-05-14 22:46:08 -0400

    Contacto Update

--- my-app/src/mainApp/screens/contacto.jsx
+++ my-app/src/mainApp/screens/contacto.jsx
@@ -1,51 +1,65 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "../style/contacto.css";
 
 function Contacto() {
   return (
-    <div id="wrapper" style={{ backgroundColor: '#a35776' }}>
-      <aside className="col-3">
-        <div className="leftMenuBar">
-          <div id="sideBar"></div>
-        </div>
-      </aside>
-
-      <div className="row col-lg-12 primary-content">
-        <div id="upBar"></div>
-        <div className="">
-          <div className="container formulary">
-            <br /><br /><br />
-            <h2>Formulario de contacto</h2>
-            <form action="/enviar-formulario" method="post">
-              <div className="form-group">
-                <label htmlFor="nombres">Nombres:</label>
-                <input type="text" className="form-control" id="nombres" name="nombres" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="apellidos">Apellidos:</label>
-                <input type="text" className="form-control" id="apellidos" name="apellidos" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="telefono">Teléfono:</label>
-                <input type="tel" className="form-control" id="telefono" name="telefono" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" className="form-control" id="email" name="email" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="ciudad">Ciudad:</label>
-                <input type="text" className="form-control" id="ciudad" name="ciudad" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="mensaje">Mensaje:</label>
-                <textarea className="form-control" id="mensaje" name="mensaje" rows="5" required></textarea>
-              </div>
-              <button type="submit" className="btn btn-primary">Enviar</button>
-            </form>
-            <br /><br />
-          </div>
-        </div>
-      </div>
+    <div className="main-contacto">
+      <Row>
+        <Container className="formulary">
+          <h2>Formulario de contacto</h2>
+          <Form action="/enviar-formulario" method="post">
+            <Form.Group>
+              <Form.Label>Nombres:</Form.Label>
+              <Form.Control
+                type="text"
+                id="nombres"
+                name="nombres"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Apellidos:</Form.Label>
+              <Form.Control
+                type="text"
+                id="apellidos"
+                name="apellidos"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Teléfono:</Form.Label>
+              <Form.Control
+                type="tel"
+                id="telefono"
+                name="telefono"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Email:</Form.Label>
+              <Form.Control type="email" id="email" name="email" required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Ciudad:</Form.Label>
+              <Form.Control type="text" id="ciudad" name="ciudad" required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Mensaje:</Form.Label>
+              <Form.Control
+                as="textarea"
+                id="mensaje"
+                name="mensaje"
+                rows="5"
+                required
+              />
+            </Form.Group>
+            <Button type="submit" className="btn btn-primary">
+              Enviar
+            </Button>
+          </Form>
+        </Container>
+      </Row>
     </div>
   );
 }
