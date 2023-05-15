4def841f22135d79ca91feb77cc155766364a742
Liân <liangrandon@gmail.com>
2023-05-14 23:32:20 -0400

    Contacto Terminado

--- my-app/src/mainApp/screens/contacto.jsx
+++ my-app/src/mainApp/screens/contacto.jsx
@@ -1,68 +1,134 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import axios from "axios";
 import "../style/contacto.css";
 
 function Contacto() {
+  const [regiones, setRegiones] = useState([]);
+  const [provincias, setProvincias] = useState([]);
+  const [comunas, setComunas] = useState([]);
+  const [provinciaSeleccionada, setProvinciaSeleccionada] = useState({});
+
+  useEffect(() => {
+    const fetchRegiones = async () => {
+      const result = await axios(
+        "https://liangrandon.000webhostapp.com/src/JSONChileData"
+      );
+      setRegiones(result.data.RegionesColeccion.Region);
+    };
+    fetchRegiones();
+  }, []);
+
+  const handleRegionChange = (e) => {
+    const regionIndex = e.target.value;
+    setProvincias(regiones[regionIndex].Provincias.Provincia);
+    setComunas([]);
+  };
+
+  const handleChange = (e) => {
+    const provinciaSeleccionada = provincias.find(
+      (provincia) => provincia.Nombre === e.target.value
+    );
+    setProvinciaSeleccionada(provinciaSeleccionada);
+    setComunas(provinciaSeleccionada.Comunas.Comuna);
+  };
+
   return (
-    <div className="main-contacto">
+    <Container>
+      <h1 className="contacto-title">Escribenos!</h1>
       <Row>
-        <Container className="formulary">
-          <br/>
-          <h2 className="contacto-title contacto-text">Formulario de contacto</h2>
-          <Form action="/enviar-formulario" method="post">
+        <Col>
+          <Form>
             <Form.Group>
-              <Form.Label className="contacto-label contacto-text">Nombres:</Form.Label>
+              <Form.Label className="contacto-label contacto-text" >Nombres:</Form.Label>
               <Form.Control
                 type="text"
                 id="nombres"
                 name="nombres"
+                placeholder="Nombre"
                 required
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label className="contacto-label contacto-text">Apellidos:</Form.Label>
+              <Form.Label className="contacto-label contacto-text" >Apellidos:</Form.Label>
               <Form.Control
                 type="text"
                 id="apellidos"
                 name="apellidos"
+                placeholder="Apellidos"
                 required
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label className="contacto-label contacto-text">Teléfono:</Form.Label>
+              <Form.Label className="contacto-label contacto-text" >Teléfono:</Form.Label>
               <Form.Control
                 type="tel"
                 id="telefono"
                 name="telefono"
+                placeholder="+569 0000 0000"
                 required
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label className="contacto-label contacto-text">Email:</Form.Label>
-              <Form.Control type="email" id="email" name="email" required />
+              <Form.Label className="contacto-label contacto-text" >Email:</Form.Label>
+              <Form.Control
+                type="email"
+                id="email"
+                name="email"
+                placeholder="correo@ejemplo.cl"
+                required
+              />
             </Form.Group>
             <Form.Group>
-              <Form.Label className="contacto-label contacto-text">Ciudad:</Form.Label>
-              <Form.Control type="text" id="ciudad" name="ciudad" required />
+              <Form.Label className="contacto-label contacto-text" >Región</Form.Label>
+              <Form.Control as="select" onChange={handleRegionChange}>
+                <option value="">Selecciona una región</option>
+                {regiones.map((region, index) => (
+                  <option key={index} value={index}>
+                    {region.Nombre}
+                  </option>
+                ))}
+              </Form.Control>
             </Form.Group>
             <Form.Group>
-              <Form.Label className="contacto-label contacto-text">Mensaje:</Form.Label>
+              <Form.Label className="contacto-label contacto-text" >Provincia</Form.Label>
+              <Form.Control as="select" onChange={handleChange}>
+                <option value="">Selecciona una provincia</option>
+                {provincias.map((provincia, index) => (
+                  <option key={index} value={provincia.Nombre}>
+                    {provincia.Nombre}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="comuna">
+              <Form.Label className="contacto-label contacto-text" >Comuna:</Form.Label>
+              <Form.Control as="select" required>
+                <option value="">Selecciona una comuna</option>
+                {comunas.map((comuna, index) => (
+                  <option key={index} value={comuna.Nombre}>
+                    {comuna.Nombre}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="message">
+              <Form.Label className="contacto-label contacto-text" >Mensaje:</Form.Label>
               <Form.Control
                 as="textarea"
-                id="mensaje"
-                name="mensaje"
-                rows="5"
+                rows={4}
+                placeholder="Escribe tu mensaje aquí"
                 required
               />
             </Form.Group>
-            <br/>
-            <Button type="submit" className="btn btn-primary">
+            <br />
+            <Button variant="primary" type="submit">
               Enviar
             </Button>
           </Form>
-        </Container>
+        </Col>
       </Row>
-    </div>
+    </Container>
   );
 }
 
